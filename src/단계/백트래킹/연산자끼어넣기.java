e4e9a52ad31eb936c6bc510758df5079aec515ff
soojung <ydyd1357@naver.com>
2025-08-12 22:54:34 +0900

    d

--- src/단계/백트래킹/연산자끼어넣기.java
+++ src/단계/백트래킹/연산자끼어넣기.java
@@ -9,60 +9,62 @@ import java.util.Arrays;
 
 public class 연산자끼어넣기 {
 	
-	public static int max = 0;
-	public static int min = 99999999;
-	public static int n;
-	public static int[] arr;
-	public static int[] map = new int[4];
+	static int max = Integer.MIN_VALUE;
+	static int min = Integer.MAX_VALUE;
+	
 	public static void main(String args[]) throws IOException {
 		BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw= new BufferedWriter(new OutputStreamWriter(System.out));
 		
-		n = Integer.parseInt(br.readLine());
-		arr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-		map = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+		int n  = Integer.parseInt(br.readLine());
+		int[] numList = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+		int[] operator = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
 		
-		dfs(arr[0], 1);
+		dp(numList[0], 1,  numList, operator);
 		
-		bw.write(max + "\n");
-		bw.write(min + "");
+		
+		System.out.println(max);
+		System.out.println(min);
 		bw.flush();
 		br.close();
 		bw.close();
 	}
-	private static void dfs(int num, int index) {
+
+	private static void dp(int num, int n, int[] numList, int[] operator) {
 		
-		if (index == n) {
+		if (n == numList.length) {
 			max = Math.max(max, num);
 			min = Math.min(min, num);
-			
-			return;
 		}
 		
-		for (int a = 0; a < 4; a ++) {
-			if ( map[a] > 0 ) {
-				map[a]--;
+		for (int a = 0; a < operator.length ; a++) {
+			int todoNum = num;
+			if (operator[a] > 0 ) {
+					switch (a) {
+					case 0: {
+						todoNum +=numList[n];
+						break;
+					}
+					case 1: {
+						todoNum -=numList[n];		
+						break;
+					}
+					case 2: {
+						todoNum *=numList[n];
+						break;
+					}
+					case 3: {
+						todoNum /=numList[n];
+						break;
+					}
+				}
 				
-				switch (a) {
-
-                case 0: //더하기일 경우
-                	dfs(num + arr[index], index + 1);
-                    break;
-                case 1: //빼기일 경우
-                	dfs(num - arr[index], index + 1);
-                    break;
-                case 2: //곱하기일 경우
-                	dfs(num * arr[index], index + 1);
-                    break;
-                case 3: //나누기일 경우
-                	dfs(num / arr[index], index + 1);
-                    break;
-
-            }
-            // 재귀호출이 종료되면 다시 해당 연산자 개수를 복구한다.
-            map[a]++;
-				
+				operator[a] = operator[a] -1;
+				dp(todoNum, n+1,  numList, operator);
+				operator[a] = operator[a] +1;
 			}
 		}
+			
 	}
+
 }
